0fc8025455967737c9fad85774b5bbce8eed30ac
Bruno Pimentel <bpimentel@mastertech.tech>
2018-06-21 12:04:01 -0300

    Criar exemplo de POST

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+app.post('/', (req, res) => {
+    if(! req.body.pudim){
+        res.send('Você não tem pudim!');
+    }else{
+        res.send(`Seu pudim é de ${req.body.pudim}`);
+    }
+    
 });
 
 app.get('/pudim', (req, res) => {
@@ -13,5 +24,6 @@ app.get('/pudim', (req, res) => {
         frescura: 'Uva Passa'
     });
 });
+
 
 app.listen(3000);
